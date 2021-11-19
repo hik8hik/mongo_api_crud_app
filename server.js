4bad21607347c1158943bd2eff8ae342981b6c28
hik8hik <hik8hik@gmail.com>
2021-11-19 15:43:20 +0000

    Created a route to render add_user

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/add-user", (req, res) => {
+  res.render("add_user");
+});
+
 //SERVER
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
