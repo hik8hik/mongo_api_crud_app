eaa6a41fe9f31117968bed23a161a9041a28a846
hik8hik <hik8hik@gmail.com>
2021-11-20 16:22:51 +0000

    Tested the response in console

--- server/services/render.js
+++ server/services/render.js
@@ -2,9 +2,15 @@ const axios = require("axios");
 
 exports.homeRoutes = (req, res) => {
   //MAKE A GET REQUEST TO THE API USERS
-  axios.get("http://localhost:3000/api/users").then(function (response) {
-    res.render("index", { users: response.data });
-  });
+  axios
+    .get("http://localhost:3000/api/users")
+    .then(function (response) {
+      console.log(response.data);
+      res.render("index", { users: response.data });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
 
 exports.add_user = (req, res) => {
