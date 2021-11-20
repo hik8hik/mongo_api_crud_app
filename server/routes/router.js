8ea269c8467ef4ebd8713c80d9e5662f3a8f40b1
hik8hik <hik8hik@gmail.com>
2021-11-20 10:16:23 +0000

    Added description to all routes

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,7 +10,17 @@ const services = require("../services/render");
 
 //Routes
 route.get("/", services.homeRoutes);
+
+/**
+ * @Description add user
+ * @method GET/ add_user
+ */
 route.get("/", services.add_user);
+
+/**
+ * @Description update user
+ * @method GET/ update_user
+ */
 route.get("/", services.update_user);
 
 module.exports = route;
