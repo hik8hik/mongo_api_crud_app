eb3957b172b27827c9ab4005d6858e63860f6fb9
hik8hik <hik8hik@gmail.com>
2021-11-20 10:21:48 +0000

    Specified gets

--- server/routes/router.js
+++ server/routes/router.js
@@ -15,12 +15,12 @@ route.get("/", services.homeRoutes);
  * @Description add user
  * @method GET/ add_user
  */
-route.get("/", services.add_user);
+route.get("/add_user", services.add_user);
 
 /**
  * @Description update user
  * @method GET/ update_user
  */
-route.get("/", services.update_user);
+route.get("/update-user", services.update_user);
 
 module.exports = route;
