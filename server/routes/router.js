2bd84793641f756e10751d3bfd47e91577a9b90f
hik8hik <hik8hik@gmail.com>
2021-11-20 09:35:43 +0000

    Changed call from app to route.get...

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,14 +2,14 @@ const express = require("express");
 const route = express.Router();
 
 //Routes
-app.get("/", (req, res) => {
+route.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/add-user", (req, res) => {
+route.get("/add-user", (req, res) => {
   res.render("add_user");
 });
 
-app.get("/update-user", (req, res) => {
+route.get("/update-user", (req, res) => {
   res.render("update_user");
 });
