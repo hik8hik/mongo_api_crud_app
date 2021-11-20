05247c5042f1679a551626151c8a10577ed539ab
hik8hik <hik8hik@gmail.com>
2021-11-20 09:58:59 +0000

    Imported services, Got homeRoutes here & Added description

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const route = express.Router();
 
+const services = require("../services/render");
+
+/**
+ * @Description Root Route
+ * @method GET/
+ */
+
 //Routes
-route.get("/", (req, res) => {
-  res.render("index");
-});
+route.get("/", services.homeRoutes);
 
 route.get("/add-user", (req, res) => {
   res.render("add_user");
