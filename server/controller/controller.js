03dc7d36fbf2d0c399f7e8661bf17abf8028312d
hik8hik <hik8hik@gmail.com>
2021-11-20 12:49:14 +0000

    REQUEST VALIDATION -- bodu should have content// New USer Creater // Save in database

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,16 +1,41 @@
 var Userdb = require("../model/model");
 
 //CREATE AND SAVE NEW USER
-exports.create = (req,res)=>{}
+exports.create = (req, res) => {
+  //VALIDATE REQUEST
+  if (req.body) {
+    res.status(400).send({ messgae: "Content Cannot Be Empty" });
+    return;
+  }
+  //NEW USER
+  const user = new Userdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
 
+  //SAVE USER IN DB
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({
+          message:
+            err.message || "Error occured while creating a create operarion",
+        });
+    });
+};
 
 //RETRIEVE | RETURN SINGLE| ALL USERS
-exports.find = (req,res)=>{}
-
+exports.find = (req, res) => {};
 
 //UPDATE USER BY ID
-exports.update = (req,res)=>{}
-
+exports.update = (req, res) => {};
 
 //DELETE USER BY ID
-exports.delete = (req,res)=>{}
+exports.delete = (req, res) => {};
