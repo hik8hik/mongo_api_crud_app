656a8f3b5ab2d86df1de28e591661613e6aa8a8d
hik8hik <hik8hik@gmail.com>
2021-11-20 09:12:59 +0000

    Server to update user

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ app.get("/add-user", (req, res) => {
   res.render("add_user");
 });
 
+app.get("/update-user", (req, res) => {
+  res.render("update_user");
+});
+
 //SERVER
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
