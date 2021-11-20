b4ee53bfd04ccec714ba0448e701847e54822ca8
hik8hik <hik8hik@gmail.com>
2021-11-20 16:23:14 +0000

    Deleted the console Tester

--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,6 @@ exports.homeRoutes = (req, res) => {
   axios
     .get("http://localhost:3000/api/users")
     .then(function (response) {
-      console.log(response.data);
       res.render("index", { users: response.data });
     })
     .catch((err) => {
