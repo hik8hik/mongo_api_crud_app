815983e71e95d4ad5e04cf088284cac91762b54c
hik8hik <hik8hik@gmail.com>
2021-11-20 12:22:12 +0000

    //CREATE AND SAVE NEW USER //RETRIEVE | RETURN SINGLE| ALL USERS //UPDATE USER BY ID //DELETE USER BY ID

--- server/routes/router.js
+++ server/routes/router.js
@@ -25,5 +25,8 @@ route.get("/update-user", services.update_user);
 
 //API
 route.post('/api/users', controller.create)
+route.get('/api/users', controller.find)
+route.put('/api/users/:id', controller.update)
+route.delete('/api/users/:id', controller.delete)
 
 module.exports = route;
