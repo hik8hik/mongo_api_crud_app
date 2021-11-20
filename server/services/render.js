8e0fabe289d54bf53b3bfa519c0e482b1887086b
hik8hik <hik8hik@gmail.com>
2021-11-20 16:16:44 +0000

    Make a GET request to the api users and pass response-data to users

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 
 exports.homeRoutes = (req, res) => {
-  res.render("index", { users: "New Data" });
+  //MAKE A GET REQUEST TO THE API USERS
+  axios.get("http://localhost:3000/api/users").then(function (response) {
+    res.render("index", { users: response.data });
+  });
 };
 
 exports.add_user = (req, res) => {
