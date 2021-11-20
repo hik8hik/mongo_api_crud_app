ee1a636dd79f816bcbc7e971e3e50eea874f5f4c
hik8hik <hik8hik@gmail.com>
2021-11-20 12:17:46 +0000

    //CREATE AND SAVE NEW USER //RETRIEVE | RETURN SINGLE| ALL USERS //UPDATE USER BY ID //DELETE USER BY ID

---
+++ server/controller/controller.js
@@ -0,0 +1,16 @@
+var Userdb = require("../model/model");
+
+//CREATE AND SAVE NEW USER
+exports.create = (req,res)=>{}
+
+
+//RETRIEVE | RETURN SINGLE| ALL USERS
+exports.find = (req,res)=>{}
+
+
+//UPDATE USER BY ID
+exports.update = (req,res)=>{}
+
+
+//DELETE USER BY ID
+exports.delete = (req,res)=>{}
