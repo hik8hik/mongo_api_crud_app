a563f8685f68c0a584131700c49d9085d31a459b
hik8hik <hik8hik@gmail.com>
2021-11-21 17:29:58 +0000

    Resolved checkbox error, added script to print unindexed array

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,37 @@
-//CONTROL THE VALUE OF CHECKBOX
-const checkbox = document.getElementById("actinaci");
+const addcheckbox = document.getElementById("puactinaci");
+const upcheckbox = document.getElementById("upactinaci");
 
-checkbox.addEventListener("change", (event) => {
-  if (event.currentTarget.checked) {
-    document.getElementById("yesnos").innerHTML = "Active";
-    document.getElementById("actinaci").value = "Active";
-  } else {
-    document.getElementById("yesnos").innerHTML = "Inactive";
-    document.getElementById("actinaci").value = "Inactive";
-  }
-});
+if (addcheckbox) {
+  //CONTROL THE VALUE OF CHECKBOX IN NEW USER
+  addcheckbox.addEventListener("change", (event) => {
+    if (event.currentTarget.checked) {
+      document.getElementById("yesnos").innerHTML = "Active";
+      document.getElementById("puactinaci").value = "Active";
+      document.getElementById("chstatus").value = "Active";
+    } else {
+      document.getElementById("yesnos").innerHTML = "Inactive";
+      document.getElementById("puactinaci").value = "Inactive";
+      document.getElementById("chstatus").value = "Inactive";
+    }
+  });
+} else if (upcheckbox) {
+  //CONTROL THE VALUE OF CHECKBOX IN UPDATE USER
+  upcheckbox.addEventListener("change", (event) => {
+    if (event.currentTarget.checked) {
+      document.getElementById("lblupactinaci").innerHTML = "Active";
+      document.getElementById("upactinaci").value = "Active";
+      document.getElementById("chstatus").value = "Active";
+      console.log("Active");
+    } else {
+      document.getElementById("lblupactinaci").innerHTML = "Inactive";
+      document.getElementById("upactinaci").value = "Inactive";
+      document.getElementById("chstatus").value = "Inactive";
+      console.log("Inactive");
+    }
+  });
+}
 
+// DO AFTER LISTENING TO SUBMIT
 $("#add_user").submit(function (event) {
   alert("Data Added Successfully");
 });
