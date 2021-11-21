a9482cb6ce3df8cf3d90d9ddcc7aecdc2f3731fb
hik8hik <hik8hik@gmail.com>
2021-11-21 04:57:34 +0000

    Want to know checkbox changing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
 $("#add-user").submit(function (event) {
   alert("Data Added Successfully");
 });
+
+//CONTROL THE VALUE OF CHECKBOX
+const checkbox = document.getElementById('actinaci')
+
+checkbox.addEventListener('change', (event) => {
+  if (event.currentTarget.checked) {
+    alert('checked');
+  } else {
+    alert('not checked');
+  }
+})
