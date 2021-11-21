fc44aba613e425fcb0af0a7f080f6f5cbd0c17b2
hik8hik <hik8hik@gmail.com>
2021-11-21 05:09:33 +0000

    Added script to change text of the checkbox-label when checked//unchecked

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,9 +8,11 @@ const checkbox = document.getElementById('actinaci')
 checkbox.addEventListener('change', (event) => {
   if (event.currentTarget.checked) {
     alert('checked');
+    document.getElementById('yesnos').innerHTML = 'Active';
     document.getElementById("actinaci").value = "Active";
 } else {
     alert('not checked');
+    document.getElementById('yesnos').innerHTML = 'Inactive';
     document.getElementById("actinaci").value = "Inactive";
   }
 })
