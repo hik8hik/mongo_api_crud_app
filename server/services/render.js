0e4ef99e89392546a6114eecd75271a9c019d5d5
hik8hik <hik8hik@gmail.com>
2021-11-21 05:20:34 +0000

    Insted of rendering update user without param id, I added ID as param and rendered it as before

--- server/services/render.js
+++ server/services/render.js
@@ -17,5 +17,7 @@ exports.add_user = (req, res) => {
 };
 
 exports.update_user = (req, res) => {
-  res.render("update_user");
+    axios.get('http://localhost:3000/api/users', {params: {id: req.query.id}}).then(function(userdata){
+        res.render('update_user', {user:userdata.data})
+    }).catch(err => {res.send(err)})
 };
